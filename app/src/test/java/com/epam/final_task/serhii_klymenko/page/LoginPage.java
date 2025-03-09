62a08378ceb54e44c0b09872600802eb0df17cec
SerhiiKlymenko <mafakka@mail.ru>
2025-03-09 12:49:42 -0400

    two more tests with hardcoded test data

--- app/src/test/java/com/epam/final_task/serhii_klymenko/page/LoginPage.java
+++ app/src/test/java/com/epam/final_task/serhii_klymenko/page/LoginPage.java
@@ -24,8 +24,8 @@ public class LoginPage extends AbstractPage {
     WebElement userPasswordInputField;
     @FindBy(css = "#login-button")
     WebElement loginButton;
-
     private final By errorMessage = By.cssSelector(".error-message-container.error h3");
+   private final By welcomeMessage = By.cssSelector("div.header_label div.app_logo");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -44,6 +44,10 @@ public class LoginPage extends AbstractPage {
         wait.until(ExpectedConditions.visibilityOf(userNameInputField)).sendKeys("random_name");
         return this;
     }
+    public LoginPage inputLegitName() {
+        wait.until(ExpectedConditions.visibilityOf(userNameInputField)).sendKeys("standard_user");
+        return this;
+    }
 
     public LoginPage clearName()  {
 //        wait.until(ExpectedConditions.elementToBeClickable(userNameInputField)).click(); //chrome autofills name after clear()
@@ -59,9 +63,16 @@ public class LoginPage extends AbstractPage {
         return this;
     }
 
+    public LoginPage inputLegitPassword()  {
+        wait.until(ExpectedConditions.visibilityOf(userPasswordInputField)).sendKeys("secret_sauce");
+        return this;
+    }
+
     public LoginPage clearPassword()  {
-        wait.until(ExpectedConditions.elementToBeClickable(userPasswordInputField)).click();
-        wait.until(ExpectedConditions.visibilityOf(userPasswordInputField)).clear();
+//        wait.until(ExpectedConditions.elementToBeClickable(userPasswordInputField)).click();
+//        wait.until(ExpectedConditions.visibilityOf(userPasswordInputField)).clear();
+        wait.until(ExpectedConditions.visibilityOf(userPasswordInputField)).sendKeys(Keys.CONTROL + "a");
+        wait.until(ExpectedConditions.visibilityOf(userPasswordInputField)).sendKeys(Keys.BACK_SPACE);
         return this;
     }
 
@@ -77,4 +88,13 @@ public class LoginPage extends AbstractPage {
     public String getErrorMessage() {
         return driver.findElement(errorMessage).getText();
     }
+     public boolean isWelcomeMessageDisplayed() {
+        return driver.findElement(welcomeMessage).isDisplayed();
+    }
+
+    public String getWelcomeMessage() {
+        return driver.findElement(welcomeMessage).getText();
+    }
+
+
 }
