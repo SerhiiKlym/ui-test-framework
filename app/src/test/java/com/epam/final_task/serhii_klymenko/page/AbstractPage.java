3d312694043a097a4a0f17da6b39e706a16d7b0a
SerhiiKlymenko <mafakka@mail.ru>
2025-03-14 00:16:28 -0400

    moved some fields to AbstractPage, InventoryPage developed, tests fixed, added locked user, added parallel execution into dataprovider

--- app/src/test/java/com/epam/final_task/serhii_klymenko/page/AbstractPage.java
+++ app/src/test/java/com/epam/final_task/serhii_klymenko/page/AbstractPage.java
@@ -2,18 +2,22 @@ package com.epam.final_task.serhii_klymenko.page;
 
 import com.epam.final_task.serhii_klymenko.driver.DriverFactory;
 import com.epam.final_task.serhii_klymenko.util.ConfigReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public abstract class AbstractPage {
-    protected WebDriver driver;
 
+    private final static Logger log = LogManager.getLogger(AbstractPage.class);
+    protected WebDriver driver;
     protected final WebDriverWait wait;
     protected String timeout = ConfigReader.get("timeout");
 
-    protected AbstractPage(){
+    protected AbstractPage() {
+        log.info("Super constructor is called.");
         this.driver = DriverFactory.getDriver();
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(Long.parseLong(timeout)));
     }
--- app/src/test/java/com/epam/final_task/serhii_klymenko/page/InventoryPage.java
+++ app/src/test/java/com/epam/final_task/serhii_klymenko/page/InventoryPage.java
@@ -5,18 +5,35 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class InventoryPage extends AbstractPage {
 
     private final static Logger log = LogManager.getLogger(InventoryPage.class);
-
     protected String inventoryUrl = ConfigReader.get("inventoryUrl");
-
-
     private final static String title = "Swag Labs";
 
     @FindBy(css = "div.header_label div.app_logo")
     WebElement inventoryDashboard;
 
+    public InventoryPage() {
+        super();
+        PageFactory.initElements(driver, this);
+    }
 
+    public boolean isInventoryPageOpened() {
+        log.info("Checking if we have opened the Inventory page for real...");
+        return (wait.until(ExpectedConditions.visibilityOf(inventoryDashboard)).isDisplayed() &&
+                (driver.getCurrentUrl().equals(inventoryUrl)));
+    }
+
+    public String getWelcomeMessage() {
+        log.info("Getting the text of a header on Inventory page...");
+        return wait.until(ExpectedConditions.visibilityOf(inventoryDashboard)).getText();
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
