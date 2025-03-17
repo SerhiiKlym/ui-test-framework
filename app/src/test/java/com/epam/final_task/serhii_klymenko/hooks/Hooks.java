52f9d206a345bc22e02e22a47b64e45f74c2faef
SerhiiKlymenko <mafakka@mail.ru>
2025-03-16 20:33:30 -0400

    screenshot utility segregation, screenshot naming

--- app/src/test/java/com/epam/final_task/serhii_klymenko/hooks/Hooks.java
+++ app/src/test/java/com/epam/final_task/serhii_klymenko/hooks/Hooks.java
@@ -1,8 +1,10 @@
 package com.epam.final_task.serhii_klymenko.hooks;
 
 import com.epam.final_task.serhii_klymenko.driver.DriverFactory;
+import com.epam.final_task.serhii_klymenko.util.ScreenshotUtil;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,7 +19,11 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            log.error("Cucumber Test Failed: " + scenario.getName());
+           ScreenshotUtil.saveScreenshot(scenario.getName());
+        }
         DriverFactory.closeDriver();
         log.info("The driver was annihilated on thread (hooks): " + Thread.currentThread().getId());
     }
